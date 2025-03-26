9c8cc2a11b01890e77a16580f2862069295c8ae6
muradhani <mouradhany574@gmail.com>
2025-03-26 11:42:22 +0200

    add new dependencies and import domain and data in app module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,10 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation ("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.11.0")
+    implementation ("com.google.dagger:hilt-android:2.56.1")
+    annotationProcessor ("com.google.dagger:hilt-compiler:2.56.1")
+    implementation(project(":domain"))
+    implementation(project(":data"))
 }
