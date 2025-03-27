25065b186834564a9f6592d170088d955f307547
muradhani <mouradhany574@gmail.com>
2025-03-27 18:07:06 +0200

    Implement RecipeListViewModel and RecipeListFragment

---
+++ app/src/main/java/com/example/newrecipeapp/viewModels/RecipeListViewModel.kt
@@ -0,0 +1,32 @@
+package com.example.newrecipeapp.viewModels
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.domain.models.RecipeEntity
+import com.example.domain.repositories.RecipeRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import jakarta.inject.Inject
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+
+@HiltViewModel
+class RecipeListViewModel @Inject constructor(
+    private val repository : RecipeRepository
+): ViewModel() {
+    private val _recipes = MutableStateFlow<List<RecipeEntity>>(emptyList())
+    val recipes: StateFlow<List<RecipeEntity>> = _recipes.asStateFlow()
+
+    init {
+        viewModelScope.launch{
+            val result = repository.search(
+                token = "Token 9c8b06d329136da358c2d00e76946b0111ce2c48",
+                page = 1 ,
+                query = "chicken"
+            )
+            _recipes.value = result!!
+        }
+    }
+}
