81689af65eeb419c5c45ef65ec8e4e7544771578
muradhani <mouradhany574@gmail.com>
2025-03-28 19:29:06 +0200

    separate food category chips and search text field into composable functions

--- app/src/main/java/com/example/newrecipeapp/fragments/RecipeListScreen.kt
+++ app/src/main/java/com/example/newrecipeapp/fragments/RecipeListScreen.kt
@@ -1,33 +1,25 @@
 package com.example.newrecipeapp.fragments
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.newrecipeapp.components.FoodCategoryChip
 import com.example.newrecipeapp.components.RecipeCard
+import com.example.newrecipeapp.components.SearchTextField
 import com.example.newrecipeapp.components.getAllFoodCategories
 import com.example.newrecipeapp.viewModels.RecipeListViewModel
 
@@ -79,38 +71,11 @@ fun RecipeListScreen(viewModel: RecipeListViewModel = hiltViewModel()) {
                                 shape = RoundedCornerShape(12.dp) // Match shadow shape
                             )
                     ) {
-                        TextField(
-                            value = query,
-                            onValueChange = { viewModel.onQueryChange(it) },
-                            placeholder = { Text("Enter text here") },
-                            modifier = Modifier
-                                .fillMaxWidth(),
-                            label = { Text(text = "Search") },
-                            keyboardOptions = KeyboardOptions(
-                                keyboardType = KeyboardType.Text,
-                                imeAction = ImeAction.Done
-                            ),
-                            leadingIcon = {
-                                Icon(
-                                    Icons.Filled.Search,
-                                    contentDescription = "Search Icon"
-                                )
-                            },
-                            keyboardActions = KeyboardActions(
-                                onDone = {
-                                    viewModel.searchRecipes(query)
-                                    keyboardController?.hide()
-                                }
-                            ),
-                            textStyle = TextStyle(color = MaterialTheme.colorScheme.onSurface),
-                            colors = TextFieldDefaults.colors(
-                                focusedContainerColor = Color.Transparent, // Make transparent to show Box background
-                                unfocusedContainerColor = Color.Transparent,
-                                disabledContainerColor = Color.Transparent,
-                                focusedIndicatorColor = Color.Transparent, // Removes the black line
-                                unfocusedIndicatorColor = Color.Transparent, // Removes the black line
-                            ),
-                            shape = RoundedCornerShape(12.dp) // TextField inner shape
+                        SearchTextField(
+                            query = query,
+                            onQueryChange = { viewModel.onQueryChange(it) },
+                            onSearch = { viewModel.searchRecipes(query) },
+                            keyboardController = keyboardController
                         )
                     }
 
@@ -125,29 +90,12 @@ fun RecipeListScreen(viewModel: RecipeListViewModel = hiltViewModel()) {
                                 .fillMaxWidth()
                         ) {
                             for (category in getAllFoodCategories()) {
-                                Box(
-                                    modifier = Modifier
-                                        .padding(end = 8.dp) // Space between items
-                                        .shadow(
-                                            elevation = 2.dp,
-                                            shape = RoundedCornerShape(8.dp)
-                                        )
-                                        .background(
-                                            color = MaterialTheme.colorScheme.surface,
-                                            shape = RoundedCornerShape(8.dp)
-                                        ).clickable(onClick = {})
-                                        .fillMaxWidth()
-                                ) {
-                                    Text(
-                                        text = category.value,
-                                        style = MaterialTheme.typography.bodyMedium,
-                                        color = MaterialTheme.colorScheme.secondary,
-                                        modifier = Modifier.padding(
-                                            horizontal = 12.dp,
-                                            vertical = 8.dp
-                                        )
-                                    )
-                                }
+                                FoodCategoryChip(
+                                    foodCategory = category,
+                                    onClick = {
+                                        viewModel.onQueryChange(category.value)
+                                    }
+                                )
                             }
                         }
                     }
