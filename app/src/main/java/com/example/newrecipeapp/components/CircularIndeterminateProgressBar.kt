3cc557ae52ba6a76a70b7801287c783b269245c0
muradhani <mouradhany574@gmail.com>
2025-03-30 17:53:54 +0200

    Implement indeterminate circular progress indicator component.

---
+++ app/src/main/java/com/example/newrecipeapp/components/CircularIndeterminateProgressBar.kt
@@ -0,0 +1,46 @@
+package com.example.newrecipeapp.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CircularInterminateProgressBar(
+    isDisplayed : Boolean
+) {
+    if (isDisplayed) {
+        Box(
+            modifier = Modifier
+                .wrapContentSize()
+                .background(Color.Transparent), // Optional: Add background if needed
+            contentAlignment = Alignment.Center
+        ) {
+            CircularProgressIndicator(
+                color = MaterialTheme.colorScheme.primary,
+                modifier = Modifier.size(50.dp)
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true, device = "spec:width=411dp,height=891dp")
+@Composable
+fun PreviewCircularIndeterminateProgressBar_FullScreen() {
+    MaterialTheme {
+        CircularInterminateProgressBar(isDisplayed = true)
+    }
+}
